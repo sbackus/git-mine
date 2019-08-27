60a586e9672d95919cd9427f77afbdc08ae50e93
Sam Backus <Samuel.Backus@gmail.com>
2019-08-27 11:23:28 -0400

    cleanup and refactor

--- src/mine.py
+++ src/mine.py
@@ -1,39 +1,24 @@
 import os
-import getpass
 from github import Github
 
+import datetime
+
+
 def run():
-    try:
-        # using an access token
-        github_access_token = os.environ['GITHUB_ACCESS_TOKEN']
-        g = Github(github_access_token)
-    except:
-        # create a github instance
-        # g = Github("user", "password")
-        print("Could not connect to github: no access token found.")
-        print("Try using username and password.")
-
-        username = input("Enter github username: ")
-        password = getpass.getpass(prompt='Enter your github password: ')
-        g = Github(username, password)
-
-    # # Then play with your Github objects:
-    # for repo in g.get_user().get_repos():
-    #     print(repo.name)
+    # connect to github
+    g = Github(os.environ['GITHUB_ACCESS_TOKEN'])
 
     org_names = ["ps-dev", "pluralsight"]
 
     for org_name in org_names:
-
         org = g.get_organization(org_name)
-
         org_repos = org.get_repos()
+
+        # count repos
         print(f'{org_name} repos:', org_repos.totalCount)
 
-        org_commits = count_commits(org_repos)
-        print(f'{org_name} commits: {org_commits}')
-
-
+        # count commits
+        print(f'{org_name} commits: {count_commits(org_repos)}')
 
 
 def count_commits(repos_list):
@@ -41,12 +26,12 @@ def count_commits(repos_list):
     print('counting commits')
     for repo in repos_list:
         print(commit_count, end="\r")
+        a_year_ago = datetime.datetime.now() - datetime.timedelta(days=365)
         try:
-            commit_count += repo.get_commits().totalCount
+            commit_count += repo.get_commits(since=a_year_ago).totalCount
         except Exception as e:
-            print("skipping repo", e)
+            print("Skipping repo: ", e)
     return commit_count
-
 
 
 if __name__ == '__main__':
